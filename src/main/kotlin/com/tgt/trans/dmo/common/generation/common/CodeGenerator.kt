ce8a6547dd0f71eebc167e92af4e5d732bca4bbe
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2023-12-08 10:36:46 -0600

    move-to-io.kotest (#3)

--- src/main/kotlin/com/tgt/trans/dmo/common/generation/common/CodeGenerator.kt
+++
@@ -1,7 +0,0 @@
-package com.tgt.trans.dmo.common.generation.common
-
-import com.tgt.trans.dmo.common.generation.kotest.KotestData
-
-interface CodeGenerator {
-    fun generateCode(data: KotestData): String
-}
--- src/main/kotlin/com/tgt/trans/dmo/common/generation/generators/actual/TypedCode.kt
+++
@@ -1,9 +0,0 @@
-package com.tgt.trans.dmo.common.generation.generators.actual
-
-import com.tgt.trans.dmo.common.generation.common.CodeSnippet
-
-data class TypedCode(
-    val simpleName: String,
-    val qualifiedName: String,
-    val code: CodeSnippet
-    )

00ca7dbf63cf2ec8f87fd29607d52575b6f01182
Alex Kuznetsov <Alex.Cue.987@gmail.com>
2023-12-08 13:23:48 -0600

    remove-old-repo (#7)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,6 @@ rootProject.name = "kotests-mockks-generator"
 
 pluginManagement {
     repositories {
-        maven(url = "https://binrepo.target.com/artifactory/tgt-repo")
-        maven(url = "https://binrepo.target.com/artifactory/platform")
-        maven(url = "https://binrepo.target.com/artifactory/gradle-cache")
-        maven(url = "https://binrepo.target.com/artifactory/TargetOSS")
-        maven(url = "https://binrepo.target.com/artifactory/transportation-dmo-prod")
-        maven(url = "https://binrepo.target.com/artifactory/maven-central")
+        mavenCentral()
     }
 }
